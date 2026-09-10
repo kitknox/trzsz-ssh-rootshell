4fe268be72f0e89e2f6dc2f5c3ede30909d6de57
Kit Knox <kit@rootshell.com>
2026-09-09 19:07:57 -0700

    Publish Apple binary package 0.2.3

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrzszSSH",
-            url: "https://github.com/kitknox/trzsz-ssh-rootshell/releases/download/v0.2.2/TrzszSSH.xcframework.zip",
-            checksum: "8290485b258da18bf5895e2ecc1c6c30d8050e6385a187a058af21402c21172c"
+            url: "https://github.com/kitknox/trzsz-ssh-rootshell/releases/download/v0.2.3/TrzszSSH.xcframework.zip",
+            checksum: "075c09b7a187c2d39109d58b8cf081dce80d156102c4db4ecbf5a731deed1aec"
         ),
         .binaryTarget(
             name: "VPNTunnel",
-            url: "https://github.com/kitknox/trzsz-ssh-rootshell/releases/download/v0.2.2/VPNTunnel.xcframework.zip",
-            checksum: "b998d313f5341db98c2b67217ce51ab46049fcb4c99c53cd6ac8f00fdd18fb0f"
+            url: "https://github.com/kitknox/trzsz-ssh-rootshell/releases/download/v0.2.3/VPNTunnel.xcframework.zip",
+            checksum: "b3b6f22e06a073554ce647b2fffd426cc098a74a9ff1f6e5e7093066d47698f7"
         ),
     ]
 )
